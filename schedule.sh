6e9b441bae77b134d9e65d2444c3d17141eb3f41
Etienne Dilocker <etienne@dilocker.de>
2021-04-17 18:06:46 +0200

    reduce irrigation time to 1min each

--- schedule.sh
+++ schedule.sh
@@ -17,7 +17,7 @@ gcloud beta scheduler jobs create http back-on-every-hour --schedule="0 8-20 * *
   --project $PROJECT \
   --time-zone $TIMEZONE
 
-gcloud beta scheduler jobs create http back-off-every-hour --schedule="2 8-20 * * *" \
+gcloud beta scheduler jobs create http back-off-every-hour --schedule="1 8-20 * * *" \
   --uri="https://api.particle.io/v1/devices/$DEVICE_ID/zone_back" \
   --headers="Authorization=Bearer $ACCESS_TOKEN,Content-Type=application/x-www-form-urlencoded" \
   --message-body="args=off" \
@@ -25,7 +25,7 @@ gcloud beta scheduler jobs create http back-off-every-hour --schedule="2 8-20 * 
   --project $PROJECT \
   --time-zone $TIMEZONE
 
-gcloud beta scheduler jobs create http side-on-every-hour --schedule="3 8-20 * * *" \
+gcloud beta scheduler jobs create http side-on-every-hour --schedule="2 8-20 * * *" \
   --uri="https://api.particle.io/v1/devices/$DEVICE_ID/zone_side" \
   --headers="Authorization=Bearer $ACCESS_TOKEN,Content-Type=application/x-www-form-urlencoded" \
   --message-body="args=on" \
@@ -33,7 +33,7 @@ gcloud beta scheduler jobs create http side-on-every-hour --schedule="3 8-20 * *
   --project $PROJECT \
   --time-zone $TIMEZONE
 
-gcloud beta scheduler jobs create http side-off-every-hour --schedule="5 8-20 * * *" \
+gcloud beta scheduler jobs create http side-off-every-hour --schedule="3 8-20 * * *" \
   --uri="https://api.particle.io/v1/devices/$DEVICE_ID/zone_side" \
   --headers="Authorization=Bearer $ACCESS_TOKEN,Content-Type=application/x-www-form-urlencoded" \
   --message-body="args=off" \
